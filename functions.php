75fe5d5f6691e1d32de085fdb71d99759463ca6f
mateuswetah <mateus.m.luna@gmail.com>
2020-09-07 21:12:18 -0300

    Creates child theme template files

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+if (! defined('WP_DEBUG')) {
+	die( 'Direct access forbidden.' );
+}
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+});
