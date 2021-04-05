25ade95228830048fea09078d42f3042bfead8a8
mateuswetah <mateus.m.luna@gmail.com>
2021-04-05 14:36:17 -0300

    Uses object instead of array for acessing media components

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,8 @@ onDocumentLoadedDo(() => {
 
 window.ctEvents.default.on('blocksy:frontend:init', () => {
 	//console.log("Resetar mais coisas aqui...", tainacan_plugin);
-    if (tainacan_plugin?.classes?.TainacanMediaGallery) {
-        (tainacan_plugin?.tainacan_media_components || []).forEach((component) => {
+    if (tainacan_plugin?.classes?.TainacanMediaGallery && tainacan_plugin?.tainacan_media_components) {
+        (Object.values(tainacan_plugin.tainacan_media_components) || []).forEach((component) => {
             new tainacan_plugin.classes.TainacanMediaGallery(
                 component.has_media_thumbs ? '#' + component.media_thumbs_id : null,
                 component.has_media_main ? '#' + component.media_main_id : null,
