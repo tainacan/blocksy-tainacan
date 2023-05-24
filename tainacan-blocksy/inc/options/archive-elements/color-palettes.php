e35bcc0b0afe1aabde36ee61284df79009051df0
mateuswetah <mateus.m.luna@gmail.com>
2023-05-24 10:04:42 -0300

    Compatibility with Blocksy 2.0.0 palettes.

--- tainacan-blocksy/inc/options/archive-elements/color-palettes.php
+++ tainacan-blocksy/inc/options/archive-elements/color-palettes.php
@@ -34,43 +34,42 @@ $options = [
 			'color6' => [ 'color' => 'var(--form-field-border-initial-color, #e0e5eb)' ],
 
 			'current_palette' => 'palette-1',
+		],
+		'palettes' => [
+			[
+				'id' => 'palette-1',
 
-			'palettes' => [
-				[
-					'id' => 'palette-1',
+				'color1' => [ 'color' => 'var(--background-color, #f8f9fb)' ],
+				'color2' => [ 'color' => 'var(--cardBackground, #ffffff)' ],
+				'color3' => [ 'color' => 'var(--cardBackground, #ffffff)' ],
+				'color4' => [ 'color' => 'var(--form-field-initial-background, #ffffff)' ],
+				'color5' => [ 'color' => 'var(--form-field-border-initial-color, #e0e5eb)' ],
+				'color6' => [ 'color' => 'var(--form-field-border-initial-color, #e0e5eb)' ],
 
-					'color1' => [ 'color' => 'var(--background-color, #f8f9fb)' ],
-					'color2' => [ 'color' => 'var(--cardBackground, #ffffff)' ],
-					'color3' => [ 'color' => 'var(--cardBackground, #ffffff)' ],
-					'color4' => [ 'color' => 'var(--form-field-initial-background, #ffffff)' ],
-					'color5' => [ 'color' => 'var(--form-field-border-initial-color, #e0e5eb)' ],
-					'color6' => [ 'color' => 'var(--form-field-border-initial-color, #e0e5eb)' ],
+			],
 
-				],
+			[
+				'id' => 'palette-2',
 
-				[
-					'id' => 'palette-2',
+				'color1' => [ 'color' => '#dfd9cd' ],
+				'color2' => [ 'color' => '#ece6db' ],
+				'color3' => [ 'color' => '#f4eee2' ],
+				'color4' => [ 'color' => '#f4eee2' ],
+				'color5' => [ 'color' => '#dfd9cd' ],
+				'color6' => [ 'color' => '#d0bf9f' ],
 
-					'color1' => [ 'color' => '#dfd9cd' ],
-					'color2' => [ 'color' => '#ece6db' ],
-					'color3' => [ 'color' => '#f4eee2' ],
-					'color4' => [ 'color' => '#f4eee2' ],
-					'color5' => [ 'color' => '#dfd9cd' ],
-					'color6' => [ 'color' => '#d0bf9f' ],
+			],
 
-				],
+			[
+				'id' => 'palette-3',
 
-				[
-					'id' => 'palette-3',
-
-					'color1' => [ 'color' => '#1e1e1e' ],
-					'color2' => [ 'color' => '#282828' ],
-					'color3' => [ 'color' => '#333333' ],
-					'color4' => [ 'color' => '#333333' ],
-					'color5' => [ 'color' => '#3eaf7c' ],
-					'color6' => [ 'color' => '#1a1a1a' ]
-					
-				]
+				'color1' => [ 'color' => '#1e1e1e' ],
+				'color2' => [ 'color' => '#282828' ],
+				'color3' => [ 'color' => '#333333' ],
+				'color4' => [ 'color' => '#333333' ],
+				'color5' => [ 'color' => '#3eaf7c' ],
+				'color6' => [ 'color' => '#1a1a1a' ]
+				
 			]
 		],
 		'sync' => '',
@@ -97,41 +96,51 @@ $options = [
 			'color3' => [ 'color' => 'var(--color, #454647)' ],
 			'color4' => [ 'color' => '#555758' ],
 			'color5' => [ 'color' => 'var(--formTextInitialColor, #454647)' ],
-			'current_palette' => 'palette-1',
-			'palettes' => [
-				[
-					'id' => 'palette-1',
-					'color1' => [ 'color' => 'var(--paletteColor1,#3eaf7c)' ],
-					'color2' => [ 'color' => 'var(--headingColor, rgba(44, 62, 80, 1))' ],
-					'color3' => [ 'color' => 'var(--color, #454647)' ],
-					'color4' => [ 'color' => '#555758' ],
-					'color5' => [ 'color' => 'var(--formTextInitialColor, #454647)' ]
+			'current_palette' => 'palette-1'
+		],
+		'palettes' => [
+			[
+				'id' => 'palette-1',
+				'color1' => [ 'color' => 'var(--paletteColor1,#3eaf7c)' ],
+				'color2' => [ 'color' => 'var(--headingColor, rgba(44, 62, 80, 1))' ],
+				'color3' => [ 'color' => 'var(--color, #454647)' ],
+				'color4' => [ 'color' => '#555758' ],
+				'color5' => [ 'color' => 'var(--formTextInitialColor, #454647)' ]
 
-				],
+			],
 
-				[
-					'id' => 'palette-2',
+			[
+				'id' => 'palette-2',
 
-					'color1' => [ 'color' => '#795040' ],
-					'color2' => [ 'color' => 'rgb(80, 54, 44)' ],
-					'color3' => [ 'color' => '#474545' ],
-					'color4' => [ 'color' => '#585655' ],
-					'color5' => [ 'color' => '#474545' ]
+				'color1' => [ 'color' => '#795040' ],
+				'color2' => [ 'color' => 'rgb(80, 54, 44)' ],
+				'color3' => [ 'color' => '#474545' ],
+				'color4' => [ 'color' => '#585655' ],
+				'color5' => [ 'color' => '#474545' ]
 
-				],
+			],
 
-				[
-					'id' => 'palette-3',
+			[
+				'id' => 'palette-3',
 
-					'color1' => [ 'color' => '#3eaf7c' ],
-					'color2' => [ 'color' => 'rgb(207, 216, 225)' ],
-					'color3' => [ 'color' => '#f0f0f0' ],
-					'color4' => [ 'color' => '#c8c8c8' ],
-					'color5' => [ 'color' => '#f0f0f0' ]
-					
-				]
+				'color1' => [ 'color' => '#3eaf7c' ],
+				'color2' => [ 'color' => 'rgb(207, 216, 225)' ],
+				'color3' => [ 'color' => '#f0f0f0' ],
+				'color4' => [ 'color' => '#c8c8c8' ],
+				'color5' => [ 'color' => '#f0f0f0' ]
+				
 			]
 		],
 		'sync' => '',
 	]
 ];
+
+/* Backwards compatibility with previous palette settings */
+if ( wp_get_theme()->get('Version') <= '1.9' ) {
+	
+	$options[$prefix . 'items_list_background_palette']['value']['palettes'] = $options[$prefix . 'items_list_background_palette']['palettes'];
+	unset($options[$prefix . 'items_list_background_palette']['palettes']);
+
+	$options[$prefix . 'items_list_text_palette']['value']['palettes'] = $options[$prefix . 'items_list_text_palette']['palettes'];
+	unset($options[$prefix . 'items_list_text_palette']['palettes']);
+}
--- tainacan-blocksy/inc/options/single-elements/document-attachments-colors.php
+++ tainacan-blocksy/inc/options/single-elements/document-attachments-colors.php
@@ -4,52 +4,59 @@ if (! isset($prefix)) {
 	$prefix = '';
 }
 
+$inner_options = [
+	$prefix . 'document_attachments_colors' => [
+		'label' => __( 'Color palette for the media above title', 'tainacan-blocksy' ),
+		'type'  => 'ct-color-palettes-picker',
+		'design' => 'block',
+		'predefined' => true,
+		'wrapperAttr' => [
+			'data-type' => 'color-palette',
+			'data-label' => 'media-component-colors'
+		],
+		'value' => [
+			'color1' => [ 'color' => 'var(--paletteColor6, #edeff2)' ],
+			'color2' => [ 'color' => 'var(--paletteColor4, #2c3e50)' ],
+			'color3' => [ 'color' => 'var(--paletteColor1, #3eaf7c)' ],
+
+			'current_palette' => 'palette-1',
+		],
+		'palettes' => [
+			[
+				'id' => 'palette-1',
+
+				'color1' => [ 'color' => 'var(--paletteColor6, #edeff2)' ],
+				'color2' => [ 'color' => 'var(--paletteColor4, #2c3e50)' ],
+				'color3' => [ 'color' => 'var(--paletteColor1, #3eaf7c)' ]
+
+			],
+
+			[
+				'id' => 'palette-2',
+
+				'color1' => [ 'color' => 'var(--paletteColor3, #415161)' ],
+				'color2' => [ 'color' => 'var(--paletteColor7, #f8f9fb)' ],
+				'color3' => [ 'color' => 'var(--paletteColor2, #33a370)' ]
+
+			]
+		],
+		'sync' => '',
+	]
+];
+
+/* Backwards compatibility with previous palette settings */
+if ( wp_get_theme()->get('Version') <= '1.9' ) {
+	
+	$inner_options[$prefix . 'document_attachments_colors']['value']['palettes'] = $inner_options[$prefix . 'document_attachments_colors']['palettes'];
+	unset($inner_options[$prefix . 'document_attachments_colors']['palettes']);
+}
+
 $options = [
 	blocksy_rand_md5() => [
 		'type' => 'ct-condition',
 		'condition' => [
 			$prefix . 'page_structure_type' => 'type-gtm'
 		],
-		'options' => [
-			$prefix . 'document_attachments_colors' => [
-				'label' => __( 'Color palette for the media above title', 'tainacan-blocksy' ),
-				'type'  => 'ct-color-palettes-picker',
-				'design' => 'block',
-				'predefined' => true,
-				'wrapperAttr' => [
-					'data-type' => 'color-palette',
-					'data-label' => 'media-component-colors'
-				],
-				'value' => [
-					'color1' => [ 'color' => 'var(--paletteColor6, #edeff2)' ],
-					'color2' => [ 'color' => 'var(--paletteColor4, #2c3e50)' ],
-					'color3' => [ 'color' => 'var(--paletteColor1, #3eaf7c)' ],
-
-					'current_palette' => 'palette-1',
-
-					'palettes' => [
-						[
-							'id' => 'palette-1',
-
-							'color1' => [ 'color' => 'var(--paletteColor6, #edeff2)' ],
-							'color2' => [ 'color' => 'var(--paletteColor4, #2c3e50)' ],
-							'color3' => [ 'color' => 'var(--paletteColor1, #3eaf7c)' ]
-
-						],
-
-						[
-							'id' => 'palette-2',
-
-							'color1' => [ 'color' => 'var(--paletteColor3, #415161)' ],
-							'color2' => [ 'color' => 'var(--paletteColor7, #f8f9fb)' ],
-							'color3' => [ 'color' => 'var(--paletteColor2, #33a370)' ]
-
-						]
-
-					]
-				],
-				'sync' => '',
-			]
-		]
+		'options' => $inner_options
     ]
 ];
